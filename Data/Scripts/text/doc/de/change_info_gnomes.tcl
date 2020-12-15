964f3933ee416259204ea7b242d355e4444809cd
Christian Voß <cech12@gmail.com>
2020-12-15 21:39:00 +0100

    fix gnome info screen for gog and steam version

--- Data/Scripts/text/doc/de/change_info_gnomes.tcl
+++ Data/Scripts/text/doc/de/change_info_gnomes.tcl
@@ -5,3 +5,11 @@ $replace
 $with
 		set imgname "/gui/icons/$color${cref}.tga"
 $end
+
+$start
+$replace
+		set imgname "/gui/icons/${cref}.tga"
+		set imgname "/texture/classicons/${cref}.tga"
+$with
+		set imgname "/gui/icons/${cref}.tga"
+$end
